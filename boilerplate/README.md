06444cae078f306557cb00c22d1b999b9a981aad
Alexander Novikov <nvkv_ai@outlook.com>
2024-02-12 22:55:27 +0300

    add boiler plate

---
+++ boilerplate/README.md
@@ -0,0 +1,5 @@
+# Boilerplate
+
+It's just a quick [boilerplate](fizz_buzz.go) for your implementation.
+
+Replace todo-comment and add a new testcase [here](tests/fizz_buzz_test.go).
---
+++ boilerplate/fizz_buzz.go
@@ -0,0 +1,22 @@
+package boilerplate
+
+import (
+	"fizz-buzz/pkg/contract"
+)
+
+type FizzBuzzer struct {
+	contract.FizzBuzzer
+	n int
+}
+
+func New(n int) contract.FizzBuzzer {
+	return &FizzBuzzer{n: n}
+}
+
+func (fb *FizzBuzzer) FizzBuzz() []string {
+	result := make([]string, 0, fb.n)
+
+	// TODO: implement your solution here
+
+	return result
+}
