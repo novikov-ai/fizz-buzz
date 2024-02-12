3394df3f7e5fb1712deb8c679ba8a1818fe51233
ainovikov <ainovikov@avito.ru>
2024-02-12 11:47:15 +0300

    add classic variant

---
+++ implementations/classic/fizz-buzz.go
@@ -0,0 +1,37 @@
+package classic
+
+import (
+	"fizz-buzz/internal/contract"
+	"strconv"
+)
+
+type FizzBuzzer struct {
+	contract.FizzBuzzer
+	n int
+}
+
+func New(n int) contract.FizzBuzzer {
+	return &FizzBuzzer{n: n}
+}
+
+func (fb *FizzBuzzer) FizzBuzz() []string {
+	result := make([]string, 0, fb.n)
+
+	for i := 1; i < fb.n; i++ {
+		printed := ""
+		if i%3 == 0 {
+			printed += "Fizz"
+		}
+		if i%5 == 0 {
+			printed += "Buzz"
+		}
+
+		if printed == "" {
+			printed = strconv.Itoa(i)
+		}
+
+		result = append(result, printed)
+	}
+
+	return result
+}
